02e4cf9f9e53a090764d30b2f5bff643538543bd
eliphan <aliceconnic@gmail.com>
2019-09-12 00:35:53 -0700

    add helper method in application controller so solidus can boot

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  class << self
+    def helper_method *meths
+      # todo: rm once solidus resolves `helper_method` issue
+    end
+
+    def layout layout, conditions = {}
+      # todo: rm once solidus resolves `layout` issue
+    end
+  end
 end
